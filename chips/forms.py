6ea5aa2e0e87ec2b5ed2b7a20813d9d9c6a753f8
Andrey Rusanov <arusanov@dyn.com>
2014-06-05 19:52:39 +0300

    Stuff

--- chips/forms.py
+++ chips/forms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.contrib.auth import authenticate
-from captcha.fields import CaptchaField
+from captcha.fields import CaptchaField, CaptchaTextInput
 
 from .models import Customer, PromoCode
 from .utils import validate_code
@@ -32,10 +32,15 @@ class CodeForm(forms.ModelForm):
 
 class RegistrationForm(forms.ModelForm):
     promo = forms.CharField(label='Введите промокод')
-    password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Повторите пароль", widget=forms.PasswordInput)
-    captcha = CaptchaField(label="Код на картинке")
-    rules_confirmation = forms.BooleanField(label='С правилами ознакомлен')
+    captcha = CaptchaField(label="Код на картинке",
+                           widget=CaptchaTextInput(
+                               attrs={'class': 'fill-field fill-field--w209 '
+                                               'fill-field--w209--type'}))
+
+    rules_confirmation = forms.BooleanField(
+        label='С условиями игры ознакомлен',
+        widget=forms.CheckboxInput(attrs={'id': 'agreeCheck',
+                                          'class': 'check-block__check'}))
 
     class Meta:
         model = Customer
@@ -55,6 +60,65 @@ class RegistrationForm(forms.ModelForm):
             "apartment": "Квартира",
             "phone": "Телефон",
             "email": "Адрес электронной почты",
+        }
+        widgets = {
+            "first_name": forms.TextInput(
+                attrs={
+                    'class': 'fill-field fill-field--w261 '
+                             'fill-field--w261--type1'}),
+
+            "last_name": forms.TextInput(
+                attrs={
+                    'class': 'fill-field fill-field--w261 '
+                             'fill-field--w261--type2'}),
+
+            "surname": forms.TextInput(
+                attrs={
+                    'class': 'fill-field fill-field--w261 '
+                             'fill-field--w261--type'}),
+
+            "post_index": forms.TextInput(
+                attrs={
+                    'class': 'fill-field fill-field--w159 '
+                             'fill-field--w159--type'}),
+            "region": forms.TextInput(
+                attrs={
+                    'class': 'fill-field fill-field--w221 fill-field-'
+                             '-w221--type drop-panel__button select'}),
+
+            "district": forms.TextInput(
+                attrs={
+                    'class': 'fill-field fill-field--w210 '
+                             'fill-field--w210--type'}),
+
+            "city": forms.TextInput(
+                attrs={
+                    'class': 'fill-field fill-field--w180 '
+                             'fill-field--w180--type'}),
+            "street": forms.TextInput(
+                attrs={
+                    'class': 'fill-field fill-field--w562 '
+                             'fill-field--w562--type'}),
+            "building": forms.TextInput(
+                attrs={
+                    'class': 'fill-field fill-field--w71 '
+                             'fill-field--w71--type'}),
+            "corpus": forms.TextInput(
+                attrs={
+                    'class': 'fill-field fill-field--w71 '
+                             'fill-field--w71--type'}),
+            "apartment": forms.TextInput(
+                attrs={
+                    'class': 'fill-field fill-field--w71 '
+                             'fill-field--w71--type'}),
+            "phone": forms.TextInput(
+                attrs={
+                'class': 'fill-field fill-field--w562 '
+                         'fill-field--w562--type'}),
+            "email": forms.EmailInput(
+                attrs={
+                    'class': 'fill-field fill-field--w562 '
+                             'fill-field--w562--type'}),
         }
 
     def clean_password2(self):
