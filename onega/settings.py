859a3238b88b05b3b2e35501351ea3e84e19118b
Andrey Rusanov <rusanov.andrey.mail@gmail.com>
2014-06-06 00:58:29 +0300

    mailgun setup

--- onega/settings.py
+++ onega/settings.py
@@ -125,6 +125,12 @@ AUTH_USER_MODEL = 'chips.Customer'
 #     }
 # }
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST_USER = 'postmaster@app26061228.mailgun.org'
+EMAIL_HOST_PASSWORD = '0j8v3jioz5n4'
+EMAIL_PORT = 587
+
 try:
     from .local_settings import *
 except ImportError:
