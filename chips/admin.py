e1e60bf5059297567d1f289fc127e448b1565721
Alexander Davydov <nyddle@gmail.com>
2014-06-07 01:21:58 +0400

    customer resource

--- chips/admin.py
+++ chips/admin.py
@@ -30,7 +30,7 @@ class SiteSettingsAdmin(admin.ModelAdmin):
     list_display = ('key', 'enabled', 'additional_data')
 
 
-class CustomerAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+class CustomerAdmin(ImportExportModelAdmin):
     list_display = ('pk', 'email', 'first_name', 'last_name', 'surname',
                     'post_index', 'region', 'district', 'city', 'street',
                     'building', 'corpus', 'apartment', 'phone', 'banks', 'is_active')
