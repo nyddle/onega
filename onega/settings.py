9f99cacc46797378604c95dbfca2db469f80fddd
Alexander Davydov <nyddle@gmail.com>
2014-06-09 01:19:00 +0400

    db from env

--- onega/settings.py
+++ onega/settings.py
@@ -89,7 +89,7 @@ REDIRECT_LOGIN_URL = 'profile'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-DATABASES = {'default': dj_database_url.config(default='postgres://uqvjzjpvptwgsq:Ho_B9vrBuDdgk3Nk19qmM4v8Ei@ec2-107-20-224-35.compute-1.amazonaws.com:5432/d5bto9e6scivbr')}
+DATABASES = {'default': dj_database_url.config(default=os.getenv('HEROKU_POSTGRESQL_JADE_URL', ''),)}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
