f23469484a23e0901c998ef6edb651a3eb35d8ba
Andrey Rusanov <arusanov@dyn.com>
2014-06-13 00:20:16 +0300

    New static page

--- chips/urls.py
+++ chips/urls.py
@@ -7,7 +7,7 @@ from .views import HomeView, ProfileView, LogoutView, Prize
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^home/$', HomeView.as_view()),
+    url(r'^home/$', HomeView.as_view(), name='another_home'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^home/(?P<method>\w+)/$', HomeView.as_view(), name='post_form_home'),
     url(r'^profile/$', ProfileView.as_view(), name='profile'),
