ee87c2890d3109b64ebb69ec5872a681f8d6c4a0
Andrey Rusanov <arusanov@dyn.com>
2014-06-21 23:41:57 +0300

    уу

--- onega/settings.py
+++ onega/settings.py
@@ -140,8 +140,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.mailgun.org'
-EMAIL_HOST_USER = 'postmaster@igra.onega.by'
-EMAIL_HOST_PASSWORD = '03usq8r9yxu0'
+EMAIL_HOST_USER = 'postmaster@app26164357.mailgun.org'
+EMAIL_HOST_PASSWORD = '5h53es29a-x0'
 EMAIL_PORT = 587
 EMAIL_FROM = 'Onega'
 
